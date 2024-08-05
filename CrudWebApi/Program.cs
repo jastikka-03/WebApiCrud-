2ebeb8b138ae10a101a10907835e86f58ac08d23
jastikka-03 <jastikka.p03@gmail.com>
2024-08-05 06:44:48 +0530

    crud api completed

--- CrudWebApi/Program.cs
+++ CrudWebApi/Program.cs
@@ -4,14 +4,25 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<EmplyoyeeDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("sqlconnectionstring")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("sqlconnectionstring")));
+
+// Add CORS services
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigins", builder =>
+    {
+        builder.AllowAnyOrigin() // Add your front-end URL here
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -20,6 +31,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Use CORS middleware
+app.UseCors("AllowSpecificOrigins");
 
 app.UseAuthorization();
 
